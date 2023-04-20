5d5cda86ec11f497ad7646da9b34a5d22847b2aa
HaiLT <l8hailt@gmail.com>
2023-04-20 18:29:28 +0700

    update feature download and share

--- lib/ui/screens/scan_result/scan_result.dart
+++ lib/ui/screens/scan_result/scan_result.dart
@@ -193,7 +193,7 @@ class _ScanResultScreenState extends State<ScanResultScreen>
 
   void _onTapBottomNavItem(int index, Pair data) async {
     switch (index) {
-      case 1:
+      case 0:
         var status = await Permission.storage.status;
         if (status.isGranted) {
           _onShowDownloadFileModal(data);
@@ -201,7 +201,7 @@ class _ScanResultScreenState extends State<ScanResultScreen>
           Permission.storage.request();
         }
         break;
-      case 2:
+      case 1:
         _onShowShareFileModal(data);
         break;
       case 3:
@@ -215,15 +215,20 @@ class _ScanResultScreenState extends State<ScanResultScreen>
         context: context,
         builder: (context) => SelectMimeTypeModal(
               title: 'Download file',
-              onTakeMimeType: (mimetype) {
+              onTakeMimeType: (mimetype) async {
                 switch (mimetype) {
                   case MimeType.pdf:
                     // PdfApi.copyFileDocumentToExternalStorage(file: data.second);
                     List<String> dataText = data.first as List<String>;
                     String dataConverted =
                         dataText.reduce((value, element) => '$value\n$element');
-                    PdfApi.saveDocumentToExternal(
+                    final result = await PdfApi.saveDocumentToExternal(
                         text: dataConverted, fileInput: data.second);
+                    if (result == null) {
+                      _onShowAlertDialog("Error");
+                    } else {
+                      _onShowAlertDialog("Success");
+                    }
                     break;
                   case MimeType.txt:
                     // TODO: Handle this case.
@@ -247,14 +252,11 @@ class _ScanResultScreenState extends State<ScanResultScreen>
               onTakeMimeType: (mimetype) async {
                 switch (mimetype) {
                   case MimeType.pdf:
-                    File file = data.second as File;
-                    Share.shareXFiles([XFile(file.path)]);
+                    PdfApi.shareDocument();
                     break;
                   case MimeType.txt:
-                    List<String> dataText = data.first as List<String>;
-                    String dataConverted =
-                        dataText.reduce((value, element) => '$value\n$element');
-                    Share.share(dataConverted);
+                    List<String> dataText = data.first;
+                    PdfApi.shareTxtFile(dataText);
                     break;
                   case MimeType.image:
                     // TODO: Handle this case.
@@ -273,5 +275,24 @@ class _ScanResultScreenState extends State<ScanResultScreen>
         EditScanResultArgument(data.first, (text) {
           scanPhotoBloc.add(ScanTextChanged(text));
         }));
+  }
+
+  void _onShowAlertDialog(String text) {
+    final alert = AlertDialog(
+      content: Text(text),
+      actions: [
+        TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("OK"))
+      ],
+    );
+    showDialog(
+      context: context,
+      builder: (context) {
+        return alert;
+      },
+    );
   }
 }
