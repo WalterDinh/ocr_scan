ceb55c8e896a2103f667dd51c8beb0680fc6ba79
HaiLT <l8hailt@gmail.com>
2023-04-20 18:29:28 +0700

    update feature download and share

--- lib/core/pdf.dart
+++ lib/core/pdf.dart
@@ -39,13 +39,20 @@ class PdfApi {
     return file;
   }
 
-  static Future shareDocument({
-    required String name,
-  }) async {
+  static Future shareDocument() async {
     final dir = await getApplicationDocumentsDirectory();
-    final filePath = '${dir.path}/$name';
+    final filePath = '${dir.path}/my_example.pdf';
 
     await Share.shareXFiles([XFile(filePath)]);
+  }
+
+  static shareTxtFile(List<String> dataText) async {
+    String dataConverted =
+        dataText.reduce((value, element) => '$value\n$element');
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final File file = File('${directory.path}/temp.txt');
+    await file.writeAsString(dataConverted);
+    await Share.shareXFiles([XFile(file.path)]);
   }
 
   static Future<File?> copyFileDocumentToExternalStorage({
@@ -66,46 +73,24 @@ class PdfApi {
     return file;
   }
 
-  // it will make a named dircotory in the internal storage and then return to its call
   static Future<File?> saveDocumentToExternal({
     required String text,
     required File fileInput,
   }) async {
-    // final pdf = Document();
-
-    // Text is added here in center
-    // pdf.addPage(
-    //   Page(
-    //     build: (context) => Text(text,
-    //         style: const TextStyle(fontSize: 24), textAlign: TextAlign.left),
-    //   ),
-    // );
-    // pdf save to the variable called bytes
-    // final bytes = await pdf.save();
     final bytes = await fileInput.readAsBytes();
 
     final timestamp = DateTime.now().millisecondsSinceEpoch;
-    final externalDir =
-        await getExternalStorageDirectory();
-    // final externalDir =
-    //     await getExternalStorageDirectories(type: StorageDirectory.documents);
+    final externalDir = Platform.isAndroid
+        ? await getExternalStorageDirectory()
+        : await getApplicationDocumentsDirectory();
+
     if (externalDir == null) {
       return null;
     }
-    // if (externalDir.isEmpty) {
-    //   return null;
-    // }
     final newPath = "${externalDir.path}/document_$timestamp.pdf";
-    // final newPath = "/storage/emulated/0/DocumentScan/document_$timestamp.pdf";
     final file = File(newPath);
-    // if (!file.parent.existsSync()) {
-    //   Directory directory = Directory(file.parent.path);
-    //   await directory.create();
-    // }
-
     await file.writeAsBytes(bytes);
 
-    // reterning the file to the top most method which is generate centered text.
     return file;
   }
 }
